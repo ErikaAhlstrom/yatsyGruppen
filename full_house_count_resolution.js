355fb645168436ca915811d975774c4e144fe39a
Maja Thunberg <majaneko@gmail.com>
2020-09-09 16:55:43 +0200

    Added full house functions

--- full_house_count_resolution.js
+++ full_house_count_resolution.js
@@ -1,19 +1,55 @@
-function countDice(dice) {
-    let values = [];
+function countDice(dice_array) {
+    let values_array = []; ////Skapa en tom array
 
-    for (let i = 0; i <= 6; i++) {
-        values[i] = 0;
+    for (let i = 0; i <= 6; i++) { //// assignar värdet 0 till varje index i values
+        values_array[i] = 0;
     }
 
-    // [0, 0, 0, 0, 0, 0, 0]
-    // [0, 1, 2, 3, 4, 5, 6]
+    // [0, 0, 0, 0, 0, 0, 0]    //// arrayen nu
+    // [0, 1, 2, 3, 4, 5, 6]    //// arrayens index
 
-    for (let current_dice of dice) {
-        values[current_dice]++;
+    for (let current_dice of dice_array) { 
+        values_array[current_dice]++;
     }
 
-    console.log(values)
+    return values_array;
+
+}
+/* 
+fuction calcFullHouse(array) */
+
+// let diceCount = countDice([5, 5, 6, 6, 6]);
+
+// console.log(diceCount);
+
+
+
+function calcFullHouse(numbers_array) {
+    let arr = countDice(numbers_array); //// skicka vidare till countDice funktionen
+    let pair = 0;
+    let three_kind = 0;
+    let full_house_score = 0;
+
+    if (arr.indexOf(2) > 0) {
+        pair = arr.indexOf(2); //// .indexOf letar efter värdet 2, returnar indexet
+    }
+    if (arr.indexOf(3) > 0) {
+        three_kind = arr.indexOf(3); //// returnar index av ev värde 3 i arrayen
+    }
+
+    if (pair > 0 && three_kind > 0) { //// om det är en kåk, spara totala summan
+        full_house_score = (pair * 2) + (three_kind * 3);
+    }
+
+    return full_house_score;    //// returnar värde 0 om ej kåk, eller summan av kåken
 
 }
 
-countDice([3, 4, 5, 5, 6]);
+// calcFullHouse([5, 5, 6, 6, 6]);
+console.log(calcFullHouse([5, 5, 6, 6, 6]));
+
+
+// const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+// console.log(beasts.indexOf('bison'));
+// expected output: 1
--- script.js
+++ script.js
@@ -37,7 +37,59 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     });
 
+    function countDice(dice_array) {
+        let values_array = []; ////Skapa en tom array
+    
+        for (let i = 0; i <= 6; i++) { //// assignar värdet 0 till varje index i values
+            values_array[i] = 0;
+        }
+    
+        // [0, 0, 0, 0, 0, 0, 0]    //// arrayen nu
+        // [0, 1, 2, 3, 4, 5, 6]    //// arrayens index
+    
+        for (let current_dice of dice_array) { 
+            values_array[current_dice]++;
+        }
+    
+        return values_array;
+    
+    }
+    
+    
+    function calcFullHouse(numbers_array) {
+        let arr = countDice(numbers_array); //// skicka vidare till countDice funktionen
+        let pair = 0;
+        let three_kind = 0;
+        let full_house_score = 0;
+    
+        if (arr.indexOf(2) > 0) {
+            pair = arr.indexOf(2); //// .indexOf letar efter värdet 2, returnar indexet
+        }
+        if (arr.indexOf(3) > 0) {
+            three_kind = arr.indexOf(3); //// returnar index av ev värde 3 i arrayen
+        }
+    
+        if (pair > 0 && three_kind > 0) { //// om det är en kåk, spara totala summan
+            full_house_score = (pair * 2) + (three_kind * 3);
+        }
+    
+        return full_house_score;    //// returnar värde 0 om ej kåk, eller summan av kåken
+    
+    }
+    
+    console.log(calcFullHouse([5, 5, 6, 6, 6]));
+
+
+    //TODO: Random dice function
+    function diceRandomizer() {
+        Math.floor(Math.random() * 7);
+    }
+
+
 });
+
+
+
 
 
 // let  = document.getElementById("headline");
