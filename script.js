c564298497108a948f8e569e0a502dedeec9809e
“erikaahlstrom” <info@erikasoldh.se>
2020-09-09 11:32:06 +0200

    Uppdaterade html med nya IDn i tablet. Skapade en js fil.

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(e){
+    let calc_button=document.getElementById("calc");
+    calc_button.addEventListener("click", function(event){
+        console.log(click);
+    });
+
+});
+
+
+// let  = document.getElementById("headline");
+// let old_text = headline_variable.innerHTML; 
+
+// console.log(old_text);
+// headline_variable.style.color = "red";
