d7c2f84323287b85b5c65736a93cfa14bcb3a4a4
Amanda Heideman <heideman.amanda@gmail.com>
2020-09-11 16:30:53 +0200

    Sum updates automatically

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    let calc_button=document.getElementById("calc");
-    calc_button.addEventListener("click", function(event){
-                                                                                
-        let p1 = document.getElementById("player1");                            
-        let p1_ones = document.getElementById("player1_ones"); ////objekt
-        let p1_twos = document.getElementById("player1_twos");
-        let p1_threes = document.getElementById("player1_threes");
-        let p1_fours = document.getElementById("player1_fours");
-        let p1_fives = document.getElementById("player1_fives");
-        let p1_sixes = document.getElementById("player1_sixes");
-        let p1_sum = document.getElementById("player1_sum");
-        let p1_bonus = document.getElementById("player1_bonus");
+    let p1 = document.getElementById("player1");                            
+    let p1_ones = document.getElementById("player1_ones"); ////objekt
+    let p1_twos = document.getElementById("player1_twos");
+    let p1_threes = document.getElementById("player1_threes");
+    let p1_fours = document.getElementById("player1_fours");
+    let p1_fives = document.getElementById("player1_fives");
+    let p1_sixes = document.getElementById("player1_sixes");
+    let p1_sum = document.getElementById("player1_sum");
+    let p1_bonus = document.getElementById("player1_bonus");
 
-        let p1_sum_array = [p1_ones.value,
-                            p1_twos.value, 
-                            p1_threes.value, 
-                            p1_fours.value, 
-                            p1_fives.value, 
-                            p1_sixes.value];   
-         ////flytta upp ovanstående "let"s till ovanför addEventListener? /M
-       
+    let p1_sum_array = [p1_ones.value,  ////Amanda: Behövs den här arrayen längre?
+                        p1_twos.value, 
+                        p1_threes.value, 
+                        p1_fours.value, 
+                        p1_fives.value, 
+                        p1_sixes.value];  
+    let sum = 0;
+    p1_ones.addEventListener("change", function(event){
            
-
-        let sum = 0;
-            for (x of p1_sum_array) {
-                
+        sum +=Number(p1_ones.value);   ////Amanda: Omvandlar p1_ones.value till number och adderar till summan
+           /* for (x of p1_sum_array) { ////Amanda: Behöver inte gå igenom hela arrayen om vi bara behöver ettorna
+                let y=Number(x);
+                console.log(y);
                 let y = parseInt(x);
                 if (isNaN(x)) {        ////Erika: Bytte ut vår "kolla om det är nummer" kod till denna. Den gamla slutatde funka
                 y = 0;
                 }
-                sum += y;
+                
             }
 
          
@@ -46,6 +43,61 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
 
     });
+    p1_twos.addEventListener("change", function(event){
+        p1_sum.innerHTML = addToSum(p1_twos.value);
+        if (sum >= 63) {
+            p1_bonus.innerHTML = 50;
+        } 
+        else {
+            p1_bonus.innerHTML = 0;
+        }
+
+    });
+    p1_threes.addEventListener("change", function(event){
+        p1_sum.innerHTML = addToSum(p1_threes.value);
+        if (sum >= 63) {
+            p1_bonus.innerHTML = 50;
+        } 
+        else {
+            p1_bonus.innerHTML = 0;
+        }
+
+    });
+    p1_fours.addEventListener("change", function(event){
+        p1_sum.innerHTML = addToSum(p1_fours.value);
+        if (sum >= 63) {
+            p1_bonus.innerHTML = 50;
+        } 
+        else {
+            p1_bonus.innerHTML = 0;
+        }
+
+    });
+    p1_fives.addEventListener("change", function(event){
+        p1_sum.innerHTML = addToSum(p1_fives.value);
+        if (sum >= 63) {
+            p1_bonus.innerHTML = 50;
+        } 
+        else {
+            p1_bonus.innerHTML = 0;
+        }
+
+    });
+    p1_sixes.addEventListener("change", function(event){
+        p1_sum.innerHTML = addToSum(p1_sixes.value);
+        if (sum >= 63) {
+            p1_bonus.innerHTML = 50;
+        } 
+        else {
+            p1_bonus.innerHTML = 0;
+        }
+
+    });
+
+    function addToSum(x){
+        sum+=Number(x);
+        return sum;
+    }
 
     function countDice(dice_array) {
         let values_array = []; ////Skapa en tom array
